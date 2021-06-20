c0bc0dbbf92659ebde13b6829c20b5558aad4414
Fatmir Gusani <fgusani@gmail.com>
2021-06-20 17:32:20 +0100

    First Commit

---
+++ Bank/Function.cpp
@@ -0,0 +1,17 @@
+//Name : Fatmir Gusani
+//Date : 20.06.2021
+
+using namespace std;
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include "Register.h"
+
+class Function
+{
+private:
+
+public:
+
+};
---
+++ Bank/Register.h
@@ -0,0 +1,16 @@
+//Name : Fatmir Gusani
+//Date : 20.06.2021
+
+#ifndef REGISTER_H
+#define REGISTER_H
+
+
+class Register {
+
+private:
+
+public:
+
+};
+
+#endif
